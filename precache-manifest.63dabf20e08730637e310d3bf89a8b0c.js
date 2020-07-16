e1cf69c4aba7f9df72faf1e663b733e0a9f8a41c
jeet <jeetbasak54@gmail.com>
2020-07-16 11:24:03 +0530

    Jeet test

---
+++ precache-manifest.63dabf20e08730637e310d3bf89a8b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e5a02904cf3a464ddb1155c5b9fe9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9b952b5f4053b6521f",
+    "url": "/static/css/main.3d343ef2.chunk.css"
+  },
+  {
+    "revision": "cad0b75107858fa56d4c",
+    "url": "/static/js/2.8ba5a246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ba5a246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9b952b5f4053b6521f",
+    "url": "/static/js/main.17736019.chunk.js"
+  },
+  {
+    "revision": "9f653ea6781261280ba6",
+    "url": "/static/js/runtime-main.5742a73a.js"
+  }
+]);
